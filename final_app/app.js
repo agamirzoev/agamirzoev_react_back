bf62f3563802672ca0b1d21dbc044da5a189c246
Renat Agamirzoev <r.agamirzoev@dunice.net>
2022-02-23 16:24:37 +0300

    Main page

--- final_app/app.js
+++ final_app/app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const newsRouter = require('./routes/news');
-
+const cors = require('cors');
 const app = express();
 
 app.use(logger('dev'));
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
